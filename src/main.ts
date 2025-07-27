a1b8c3fda39d0b10a534fc76c040f66a981c47d0
Đặng Tuấn Kiệt <kietdang1608@gmail.com>
2025-07-27 19:28:45 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
+  // ✅ Enable CORS trước khi start server
   app.enableCors({
-    origin: ["http://localhost:3000", "https://atvsld-fe.vercel.app"],
-
+    origin: ['http://localhost:3000', 'https://atvsld-fe.vercel.app'],
     credentials: true,
   });
-}
 
+  await app.listen(process.env.PORT ?? 3000);
+}
 bootstrap();
