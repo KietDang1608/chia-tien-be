ed77b34ef2d5adc49221d0faf47c3a43c92dea80
Đặng Tuấn Kiệt <kietdang1608@gmail.com>
2025-07-27 19:46:48 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // ✅ Enable CORS trước khi start server
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://atvsld-fe.vercel.app'],
+    origin: ['http://localhost:3000', 'https://chia-tien-iota.vercel.app'],
     credentials: true,
   });
 
