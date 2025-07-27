749bcd00c173c855d02a20587f8f15df2575ae51
Đặng Tuấn Kiệt <kietdang1608@gmail.com>
2025-07-27 19:05:57 +0700

    abc

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfiguration } from './configurations/database.configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MembersModule],
+  imports: [MembersModule
+    , TypeOrmModule.forRoot(DatabaseConfiguration.getDatabaseConfig()),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -8,6 +8,6 @@ import { Member } from './entities/member.entity';
   imports: [TypeOrmModule.forFeature([Member])],
   controllers: [MembersController],
   providers: [MembersService],
-  exports: [MembersService, TypeOrmModule.forFeature([Member])],
+  exports: [MembersService, TypeOrmModule],
 })
 export class MembersModule {}
